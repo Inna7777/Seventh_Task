a1b13b4b958523d2d93ab3f3ca679b6fd6efb0a6
Inna7777 <peshkova-inna@mail.ru>
2022-12-21 20:04:58 +0400

    Add the new task about array

---
+++ Task01/Program.cs
@@ -0,0 +1,54 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+double[,] TowArray(int rows, int columns)
+{
+    double[,] array = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.NextDouble()*10;
+        }
+    } 
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]:F2} ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+int rows = GetNumber("Введите количество строк:");
+int columns = GetNumber("Введите количество столбцов:");
+double[,] array = TowArray(rows, columns);
+PrintArray(array);
